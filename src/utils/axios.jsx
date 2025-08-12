adb48b474ff85b90ab4996cd0632dc09776abecb
Bharadwaj-Aapmor <abharadwaj@aapmor.com>
2025-08-12 12:46:34 +0530

    Update axios.jsx

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://healthcure-backend-deebgrg0eaffbmgv.canadacentral-01.azurewebsites.net/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
